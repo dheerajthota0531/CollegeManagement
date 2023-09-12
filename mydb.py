4f62290ddadb32f58cb58c822d0253e3a416334b
dheerajthota0531 <dheeraj.thota.0531@gmail.com>
2023-09-12 19:29:00 +0530

    initial commit

---
+++ mydb.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+
+database = mysql.connector.connect(
+    host ="localhost", 
+    user ='root',
+    passwd ='0531'
+
+)
+
+
+cursorObject = database.cursor()
+
+
+
+cursorObject.execute("CREATE DATABASE studentdb")
+
+print("All Good")
